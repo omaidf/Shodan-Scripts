06f63863296f8c84f7fa4a3bf0e61c6f9ada5cc8
Omaid Faizyar <11565994+omaidf@users.noreply.github.com>
2017-11-30 14:07:10 -0800

    init 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shodan Scripts
+
+### Work in Progress
---
+++ search.py
@@ -0,0 +1,31 @@
+import shodan
+import ConfigParser
+config = ConfigParser.RawConfigParser()
+config.read("./config.ini")
+
+apikey = config.get('shodan', 'apikey')
+api = shodan.Shodan(apikey)
+ignoredports = [80,443,8080,8081]
+
+def searchip(ip):
+	host = api.host(ip)
+	for item in host['data']:
+		if item['port'] not in ignoredports:
+		        print """
+		                Port: %s
+		                Banner: %s
+
+		        """ % (item['port'], item['data'])
+
+def searchorg(org):
+	try:
+	        results = api.search(org)
+	        print 'Results found: %s' % results['total']
+	        for result in results['matches']:
+	                print 'IP: %s' % result['ip_str']
+	                print ''
+	                searchip(result['ip_str'])
+	except shodan.APIError, e:
+	        print 'Error: %s' % e
+
+searchorg('GAP Inc')
