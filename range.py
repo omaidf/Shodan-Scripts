ae46d4ab50615e62dda64ab5145b896ceaa421b4
Omaid Faizyar <11565994+omaidf@users.noreply.github.com>
2017-11-30 15:30:20 -0800

    added range search

---
+++ range.py
@@ -0,0 +1,24 @@
+import shodan
+import ConfigParser
+from IPy import IP
+config = ConfigParser.RawConfigParser()
+config.read("./config.ini")
+apikey = config.get('shodan', 'apikey')
+api = shodan.Shodan(apikey)
+
+def searchip(ip):
+	host = api.host(ip)
+	for item in host['data']:
+		if item['port'] not in ignoredports:
+		        print """
+		                Port: %s
+		                Banner: %s
+
+		        """ % (item['port'], item['data'])
+
+ip2 = IP('8.18.111.0/24')
+print
+for x in ip2:
+	print x
+	searchip(x)
+
